37f36250fcfc7298524cec304fe8cf4594d3720a
Ken Collins <ken@metaskills.net>
2012-03-26 20:09:01 -0400

    Abstract out #load_seed and use proper rake hook.

--- lib/named_seeds/railtie.rb
+++ lib/named_seeds/railtie.rb
@@ -20,13 +20,33 @@ module NamedSeeds
     end
     
     def load_seed
-      # Reset the database.
+      setup_test_environment
+      clean_test_database
+      load_all_seeds
+    end
+    
+    
+    protected
+    
+    def setup_test_environment
+      unless Rails.env.test?
+        ActiveRecord::Base.clear_all_connections!
+        ActiveRecord::Base.configurations.clear
+        silence_warnings { Object.const_set :RAILS_ENV, 'test' ; ENV['RAILS_ENV'] = 'test' ; Rails.instance_variable_set :@_env, nil }
+        ActiveRecord::Base.configurations = Rails.configuration.database_configuration
+        ActiveRecord::Base.establish_connection
+      end
+    end
+    
+    def clean_test_database
       require 'database_cleaner'
       DatabaseCleaner.logger = Rails.logger
       cleaner_opts = config.named_seeds.db_cleaner
       cleaner = DatabaseCleaner[cleaner_opts.orm, {:connection => cleaner_opts.connection}]
       cleaner.clean_with cleaner_opts.strategy, cleaner_opts.strategy_args
-      # Load all seeds.
+    end
+    
+    def load_all_seeds
       Rails.application.load_seed if config.named_seeds.app_load_seed
       config.named_seeds.engines_with_load_seed.each { |engine| engine.load_seed }
       seed_file = Rails.application.paths["db/test/seeds"].existent.first
--- lib/named_seeds/railties/databases.rake
+++ lib/named_seeds/railties/databases.rake
@@ -4,21 +4,10 @@ namespace :db do
     desc "Run the seed data from db/test/seeds.rb"
     task :seed => :environment do
       Rake::Task["db:abort_if_pending_migrations"].invoke
-      Rake::Task["db:test:_setup_test_environment_for_named_seeds"].invoke
       NamedSeeds::Railtie.load_seed
-    end
-
-    task :_setup_test_environment_for_named_seeds do
-      # unless Rails.env.test?
-        ActiveRecord::Base.clear_all_connections!
-        ActiveRecord::Base.configurations.clear
-        silence_warnings { Object.const_set :RAILS_ENV, 'test' ; ENV['RAILS_ENV'] = 'test' ; Rails.instance_variable_set :@_env, nil }
-        ActiveRecord::Base.configurations = Rails.configuration.database_configuration
-        ActiveRecord::Base.establish_connection
-      # end
     end
 
   end
 end
 
-task 'db:test:prepare' => 'db:test:seed'
+task 'test:prepare' => 'db:test:seed'
